1b138478cc7f2a439684a456799e2729809d9ce1
htetko <htetko.connetis@gmail.com>
2022-11-03 21:16:00 +0630

    Code Refectoring and testing callback hell

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let request = new XMLHttpRequest();
+/* ---- Fetch Api Old style Version ------*/
+
+const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange',function(){
     if(this.readyState === 4 && this.status === 200) {
@@ -10,3 +12,42 @@ request.addEventListener('readystatechange',function(){
 
 request.open('GET','test.json');
 request.send();
+
+/* ---- /Fetch Api Old style Version ------*/
+
+
+/* ---- Code Refectoring Fetch Api Old style Version ------*/
+
+const getRequest = (resource,callback)=> {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange',function(){
+        if(this.readyState === 4 && this.status === 200) {
+            const datas = JSON.parse(request.responseText);
+            callback(datas,undefined);
+        }else if(this.status === 404) {
+            callback(undefined,'your link is not avalaible');
+        }
+    });
+
+    request.open('GET',resource);
+    request.send();
+}
+
+getRequest("test.json",(data,err)=> {
+    if(data) {
+        console.log(data);
+        /* this style is callback hell repeat function call */
+        getRequest("test2.json",(data,err)=> {
+            if(data) {
+                console.log(data);
+            }else {
+                console.log(err);
+            }
+        })
+    }else {
+        console.log(err);
+    }
+});
+
+/* ---- /Code Refectoring Fetch Api Old style Version ------*/
